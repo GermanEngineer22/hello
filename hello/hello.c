b07f583287730031c6cc82c323eb6f1f3b8f0f96
Phillip Schwab <44279028+GermanEngineer22@users.noreply.github.com>
2019-01-17 11:17:59 -0600

    Add files via upload

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Phillip Schwab CJai Payne
+ * Date: 2019/15/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Phillip Schwab CJai Payne\n");
+
+  return 0;
+}
